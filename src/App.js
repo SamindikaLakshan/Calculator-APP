b308564af75f9626cc5f4a51a050f3d03ac1af6f
samindikaLakshan <samindikalakshan71@gmail.com>
2025-02-26 01:49:03 +0530

    create front end
    Created a front end for the calculator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="Cal-grid">
       <div className="Output">
-        <div className="Previous"
+        <div className="Previous-output"></div>
+        <div className="Current-output"></div>
+
       </div>
+
+        <button className='Span-two'>AC</button>
+        <button className='Span-two'>DEL</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className='Op-btn'>+</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button className='Op-btn'>-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button className='Op-btn'>x</button>
+        <button>.</button>
+        <button>0</button>
+        <button>=</button>
+        <button className='Op-btn'>/</button>
+        
+     
+      
     </div>
   );
 }
