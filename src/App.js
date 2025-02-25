cb6d7743955746093d9e382c7b2780d0536f57ab
samindikaLakshan <samindikalakshan71@gmail.com>
2025-02-25 23:57:34 +0530

    initiate project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Cal-grid">
+      <div className="Output">
+        <div className="Previous"
+      </div>
+    </div>
+  );
+}
+
+export default App;
